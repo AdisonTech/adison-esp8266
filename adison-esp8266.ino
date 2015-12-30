0471e89b5d5f65e7038fa0f15dd02d6ec538b583
Cliff Brake <cbrake@bec-systems.com>
2015-12-29 21:19:04 -0500

    WIP

--- adison-esp8266.ino
+++ adison-esp8266.ino
@@ -7,7 +7,7 @@
  */
 
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <ESP8266HTTPClient.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
@@ -20,7 +20,6 @@ DHT dht(5, DHT22);
 const char* mqtt_server = "portal.adisontech.com";
 
 WiFiClient espClient;
-PubSubClient client(espClient);
 
 long lastMsg = 0;
 char msg[50];
@@ -33,22 +32,6 @@ void clear_config() {
 
 byte mac_[6];
 char mac[14];
-
-String topicBase;
-String topicTemp;
-String topicHumid;
-String topicPing;
-
-void setup_mqtt() {
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  topicBase = "node/" + String(mac);
-  topicTemp = topicBase + "/temp";
-  topicHumid = topicBase + "/humidity";
-  topicPing = topicBase + "/ping";
-
-  Serial.println(topicBase);
-}
 
 StaticJsonBuffer<200> jsonBuffer;
 
@@ -97,33 +80,12 @@ void setup() {
   WiFiManager wifiManager;
   wifiManager.autoConnect("AutoConnectAP");
   Serial.println("connected...yeey :)");
-
-  setup_mqtt();
 }
 
 void blink() {
   digitalWrite(BUILTIN_LED, 0);
   delay(100);
   digitalWrite(BUILTIN_LED, 1);
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
 }
 
 void reconnect() {
@@ -164,15 +126,7 @@ void loop() {
       Serial.print(" %\t");
       Serial.print("Temperature: ");
       Serial.println(t);
-
-      client.publish(topicTemp.c_str(), String(t).c_str());
-      client.publish(topicHumid.c_str(), String(h).c_str());
     }
-    lastMsg = now;
-    ++value;
-    Serial.print("Sending ping ");
-    Serial.println(value);
-    client.publish(topicPing.c_str(), String(value).c_str());
 
     Serial.print("Free Heap: ");
     Serial.println(ESP.getFreeHeap());
